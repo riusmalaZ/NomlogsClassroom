0324c11da1aaaebeb25c28146b4847e1f48e3b43
jaeparc <tgarnon45@gmail.com>
2025-01-24 18:44:36 +0100

    freeze bullet

--- Assets/Scripts/BulletLife.cs
+++ Assets/Scripts/BulletLife.cs
@@ -2,20 +2,39 @@ using UnityEngine;
 
 public class BulletLife : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float TimeAfterFirstHitBeforeFreeze;
 
-    // Update is called once per frame
+    private float _timerAfterHit;
+    private bool _hasHit;
+    private bool _waitToFreeze;
+    private bool _isFrozen;
+
     void Update()
     {
-        
+        _timerBeforeFreeze();
+    }
+
+    void _timerBeforeFreeze(){
+        if(_hasHit && !_isFrozen){
+            _timerAfterHit+= Time.deltaTime;
+            if(_timerAfterHit >= TimeAfterFirstHitBeforeFreeze){
+                _waitToFreeze = true;
+            }   
+        }
+    }
+
+    void _freeze(){
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        GetComponent<SphereCollider>().enabled = false;
+        _isFrozen = true;
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        _hasHit = true;
+        if(_waitToFreeze && collision.gameObject.CompareTag("Sol"))
+            _freeze();
+
         Debug.Log("Bullet hit gumsssss");
 
         if (collision.gameObject.tag == "Gum")
