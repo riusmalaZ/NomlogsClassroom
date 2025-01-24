7fd6c27d87445dfa0c525608c65d5be9503dadf6
Vico2440 <114407175+Vico2440@users.noreply.github.com>
2025-01-24 10:18:33 +0100

    Student Generator

---
+++ Assets/StudentGenerator.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentGenerator : MonoBehaviour
+{
+
+    public GameObject[] Emplacement = new GameObject[16];
+
+    [Range(0, 16)]
+    public int NombreEtudiant;
+
+
+    public GameObject studentPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateStudent(NombreEtudiant, studentPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Generate a random number of students
+    public void GenerateStudent(int NombreEtudiant, GameObject Prefab)
+    {
+        List<int> randomList = new List<int>();
+
+        for (int i = 0; i < NombreEtudiant; i++)
+        {
+            
+            int random = Random.Range(0, 16);
+
+            while(randomList.Contains(random))
+            {
+                random = Random.Range(0, 16);
+            }
+
+            if(!randomList.Contains(random))
+            {
+                GameObject student = Instantiate(Prefab, Emplacement[random].transform.position, Quaternion.Euler(0, 0, 90));
+                student.transform.SetParent(this.transform);
+                randomList.Add(random);
+            }
+
+            
+        }
+    }
+}
